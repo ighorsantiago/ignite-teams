e214e23444b8bb662c25bd92c70bed58d6bfa9ce
Ighor Santiago <ighor_santiago@hotmail.com>
2023-07-10 12:44:21 -0300

    Ignite Teams

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { Container, LoadIndicator } from "./styels";
+
+export function Loading() {
+
+    return (
+        <Container>
+            <LoadIndicator />
+        </Container>
+    );
+}
